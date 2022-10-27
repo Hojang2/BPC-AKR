cb8c88a9e2db319eab93b1f9b1e13783fe4f695a
xbuchm01 <230750@vut.cz>
2022-10-27 22:30:10 +0200

    Added system for exchangin money

--- Seminar5.py
+++ Seminar5.py
@@ -32,38 +32,7 @@ class Block(object):
         self.hash = hash
 
 
-difficulty = 4
-blocks = [Block("Hello, I'm first block", "0")]
-t1 = time.time()
-blocks[0].mine_block(difficulty)
-print("Block vytezen po:", time.time() - t1)
-
-print(blocks[0].to_json())
-for i in range(10):
-    block = Block("Hello, I'm first block", blocks[-1].hash)
-    t1 = time.time()
-    block.mine_block(difficulty)
-    print("Block vytezen po:", time.time() - t1)
-    blocks.append(block)
-
-difficulty = 6
-blocks = [Block("Hello, I'm first block", "0")]
-t1 = time.time()
-blocks[0].mine_block(difficulty)
-print("Block vytezen po:", time.time() - t1)
-
-for i in range(10):
-    block = Block("Hello, I'm first block", blocks[-1].hash)
-    t1 = time.time()
-    block.mine_block(difficulty)
-    print("Block vytezen po:", time.time() - t1)
-    blocks.append(block)
-
-
 # Bitcoin momentalne obsahuje 17 nul
-
-for block in blocks:
-    print(block.to_json())
 
 
 def is_chain_valid(blockchain) -> bool:
@@ -83,11 +52,128 @@ def is_chain_valid(blockchain) -> bool:
     return True
 
 
-print("Is chain valid: ", is_chain_valid(blocks))
+class Wallet:
+    def __init__(self, name):
+        self.name = name
+        self.UTXOs = []
 
-blocks[1].data = "New data"
-print(blocks[1].to_json())
+    def __str__(self):
+        return self.to_json()
 
-print("Is chain valid: ", is_chain_valid(blocks))
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+    def send_funds(self, recipient, value):
+        bill = value
+        neededUTXOs = []
+        for utxo in self.UTXOs:
+            if bill > 0:
+                bill -= utxo.UTXO
+                neededUTXOs.append(utxo)
+        transaction = Transaction(self.name, recipient.name, value, neededUTXOs)
+        for UTXO in neededUTXOs:
+            self.UTXOs.remove(UTXO)
+        self.UTXOs.append(TransactionInput(transaction.outputs[0]))
+        recipient.UTXOs.append(TransactionInput(transaction.outputs[1]))
+        return transaction
 
 
+class Transaction:
+    def __init__(self, sender, recipient, value, inputs):
+        self.sender = sender
+        self.recipient = recipient
+        self.value = value
+        self.id = self.calculate_hash()
+        self.inputs = inputs
+        self.outputs = self.processTransaction()
+
+    def calculate_hash(self):
+        text = f"{self.sender}{self.recipient}{self.value}"
+        return hashlib.sha256(text.encode('utf-8')).hexdigest()
+
+    def __str__(self):
+        return self.to_json()
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+    def processTransaction(self):
+        coinsInInputs = 0
+        for input in self.inputs:
+            coinsInInputs += input.UTXO
+        if coinsInInputs < self.value:
+            raise ValueError("Nedostatek mincí pro provedení transakce!")
+        cashback = coinsInInputs - self.value
+        return [TransactionOutput(self.sender, cashback, self.id),
+                TransactionOutput(self.recipient, self.value, self.id)]
+
+
+class TransactionInput:
+    def __init__(self, transactionOutput):
+        self.UTXO = transactionOutput.value
+        self.transactionOutputId = transactionOutput.id
+
+    def __str__(self):  # formátování do JSON
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+
+class TransactionOutput:
+    def __init__(self, recipient, value, parent_transaction_id):
+        self.recipient = recipient
+        self.value = value
+        self.parentTransactionId = parent_transaction_id
+        self.id = self.calculate_hash()
+
+    def __str__(self): #formátování do JSON
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+    def calculate_hash(self):
+        text = f"{self.recipient}{self.value}{self.parentTransactionId}"
+        return hashlib.sha256(text.encode('utf-8')).hexdigest()
+
+
+w1 = Wallet("Jukemi")
+w2 = Wallet("Waldo")
+
+t1 = Transaction("Satoshi Nakamoto", "Jukemi", 100, [TransactionInput(TransactionOutput("0", 100, "0"))])
+
+w1.UTXOs = [TransactionInput(t1.outputs[1])]
+
+difficulty = 4
+blockchain = [Block(t1, "0")]
+time1 = time.time()
+blockchain[0].mine_block(difficulty)
+print("Block vytezen po:", time.time() - time1)
+print("Is chain valid: ", is_chain_valid(blockchain))
+
+t2 = w1.send_funds(w2, 80)
+
+blockchain.append(Block(t2, blockchain[-1].hash))
+time1 = time.time()
+blockchain[-1].mine_block(difficulty)
+print("Block vytezen po:", time.time() - time1)
+print("Is chain valid: ", is_chain_valid(blockchain))
+
+t3 = w2.send_funds(w1, 30)
+
+blockchain.append(Block(t3, blockchain[-1].hash))
+time1 = time.time()
+blockchain[-1].mine_block(difficulty)
+print("Block vytezen po:", time.time() - time1)
+print("Is chain valid: ", is_chain_valid(blockchain))
+
+#for block in blockchain:
+#    print(block.to_json())
+print(w1)
+print(w2)
+
+# pouziti neexistujicich penez vyvola vyjimku
+"""
+t4 = w2.send_funds(w1, 100)
+blockchain.append(Block(t4, blockchain[-1].hash))
+time1 = time.time()
+blockchain[-1].mine_block(difficulty)
+print("Block vytezen po:", time.time() - time1)
+print(blockchain[0].to_json())
+print("Is chain valid: ", is_chain_valid(blockchain))
+"""
